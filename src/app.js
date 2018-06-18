62b475518aa848658f3157949939fcd82fd5c68d
kiyoshimori <Darkf0x13566@gmail.com>
2018-06-19 01:59:10 +0300

    add hot reload

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { AppContainer } from 'react-hot-loader'
+
+import Client from './client';
+
+const render = Component => {
+	ReactDOM.render(
+		<AppContainer>
+			<Component />
+		</AppContainer>,
+		document.getElementById("root")
+	);
+};
+
+render(Client);
+
+if (module.hot) {
+	module.hot.accept('./client', () => {
+		const newClient = require('./client').default;
+		render(newClient);
+	});
+}
---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import React, { PureComponent } from 'react';
+
+export default class extends PureComponent {
+	render() {
+		return <h1>TEST123</h1>
+	}
+}
